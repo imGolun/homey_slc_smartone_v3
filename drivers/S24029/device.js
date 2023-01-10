0e696bc5ae965327a07136ed3375cf394a01041b
imGolun <83078279@qq.com>
2023-01-10 18:01:29 +0800

    Add support for S24029

---
+++ drivers/S24029/device.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const SrZigbeeLight = require('../../lib/SrZigbeeLight')
+    
+
+class S24029_DimLight extends SrZigbeeLight {
+     
+}
+
+module.exports = S24029_DimLight
---
+++ drivers/S24029/driver.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const SrZigBeeDriver = require('../../lib/SrZigBeeDriver')
+
+class S24029_Driver extends SrZigBeeDriver {
+
+  onInit () {
+    super.onInit()
+ 
+  }
+
+}
+
+module.exports = S24029_Driver
